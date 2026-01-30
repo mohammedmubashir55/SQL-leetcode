180c5fa43d51c404909f17d84c1c2022c1842b91
Mohammed Mubashir <mohammedmubashir175@gmail.com>
2026-01-30 15:12:30 +0530

    Time: 219 ms (80.55%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+select query_name ,
+round (avg (rating::numeric /position),2)as quality, round (
+    sum (case when rating <3 then 1 else 0 end )::numeric * 100 /count (*),2)
+    as poor_query_percentage 
+from queries 
+group by query_name ;
