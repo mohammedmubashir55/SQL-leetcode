1ce26a94c8637f03d701f6478df3cf5fdede3468
Mohammed Mubashir <mohammedmubashir175@gmail.com>
2026-01-31 10:28:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,11 @@
+SELECT
+    TO_CHAR(trans_date, 'YYYY-MM') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    COUNT(*) FILTER (WHERE state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(amount) FILTER (WHERE state = 'approved') AS approved_total_amount
+FROM Transactions
+GROUP BY
+    TO_CHAR(trans_date, 'YYYY-MM'),
+    country;
